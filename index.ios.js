73ee18fc24b8a2c5ce0240dc7a1bedb76ca61691
Marcus Egues <marcusegues@gmail.com>
2017-06-20 13:53:33 +0200

    Start implementing webrtc functionality

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,49 @@
 import { AppRegistry } from 'react-native';
 import AppComponent from './src/index';
+import WebRTC from 'react-native-webrtc';
+const {
+  RTCPeerConnection,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  MediaStream,
+  MediaStreamTrack,
+  getUserMedia,
+} = WebRTC;
 
-AppRegistry.registerComponent('BasicExamplePjSip', () => AppComponent);
+let localStream;
+
+function getLocalStream(isFront, callback) {
+
+  let videoSourceId;
+
+  // on android, you don't have to specify sourceId manually, just use facingMode
+  // uncomment it if you want to specify
+  if (Platform.OS === 'ios') {
+    MediaStreamTrack.getSources(sourceInfos => {
+      console.log("sourceInfos: ", sourceInfos);
+
+      for (const i = 0; i < sourceInfos.length; i++) {
+        const sourceInfo = sourceInfos[i];
+        if(sourceInfo.kind == "video" && sourceInfo.facing == (isFront ? "front" : "back")) {
+          videoSourceId = sourceInfo.id;
+        }
+      }
+    });
+  }
+  getUserMedia({
+    audio: true,
+    video: {
+      mandatory: {
+        minWidth: 640, // Provide your own width, height and frame rate here
+        minHeight: 360,
+        minFrameRate: 30,
+      },
+      facingMode: (isFront ? "user" : "environment"),
+      optional: (videoSourceId ? [{sourceId: videoSourceId}] : []),
+    }
+  }, function (stream) {
+    console.log('getUserMedia success', stream);
+    callback(stream);
+  }, logError);
+}
